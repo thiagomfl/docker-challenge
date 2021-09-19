48125af01f29cad5742a798f11294a5f5fc72e4e
Thiago Moura <thiagomf@motorola.com>
2021-09-19 11:58:39 -0300

    test: add test to application

---
+++ main_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"testing"
+	"net/http"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/stretchr/testify/require"
+)
+
+
+/*
+	- The tests in this suite check if the business requirements have been satisfied. Each test checks a single requirement.
+	- Every test in this suite is isolated from all other tests, there is no data or container instances shared at all.
+	- Beware, that by default HTTP client does not set a timeout. In tests, however, this isn't an issue because the tests *do* have a timeout, which defaults to 10 minutes, but can be reduced vai a command-line parameter.
+	- After a container has started, there is some time required before the application inside it is ready to service the incoming requests. This creates a race condition with the test. To avoid this, the tests retry with exponential back-off.
+*/
+
+// Requirement #1: The application response must contain a heart ("<3")
+func TestRespondsWithLove(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err, "Could not connect to Docker")
+
+	resource, err := pool.Run("thienry/fullcycle", "latest", []string{})
+	require.NoError(t, err, "Could not start container")
+
+	t.Cleanup(func() {
+		require.NoError(t, pool.Purge(resource), "Failed to remove container")
+	})
+
+	var resp *http.Response
+
+	err = pool.Retry(func() error {
+		resp, err := http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/"))
+		if err != nil {
+			t.Log("Container not ready, waiting...")
+			return err
+		}
+
+		resp.Body.Close()
+		return nil
+	})
+	require.NoError(t, err, "HTTP error")
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "Failed to read HTTP body")
+
+	require.Contains(t, string(body), "<3", "Does not respond with love?")
+}
+
+// Requirement #2: The application must include a health-check endpointat /ping,
+// responding with JSON document {"Status": "OK"} if everything is well.
+func TestHealthCheck(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err, "Could not connect to Docker")
+
+	resource, err := pool.Run("thienry/fullcycle", "latest", []string{})
+	require.NoError(t, err, "Could not start container")
+
+	t.Cleanup(func() {
+		require.NoError(t, pool.Purge(resource), "Failed to remove container")
+	})
+
+	var resp *http.Response
+
+	err = pool.Retry(func() error {
+		resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/ping"))
+		if err != nil {
+			t.Log("Container not ready, waiting...")
+			return err
+		}
+
+		return nil
+	})
+	require.NoError(t, err, "HTTP error")
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "Failed to read HTTP body")
+
+	require.JSONEq(t, `{"Status": "OK"}`, string(body), "Does not respond with valid JSON?")
+}
